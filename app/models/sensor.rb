a5ae45f220853fcc4f61e3674ff9432904d847a5
Felix <FelixinLondon@googlemail.com>
2020-11-09 23:44:57 +0100

    Model Work

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,6 +1,6 @@
 class Sensor < ApplicationRecord
     validates_presence_of :hardware_id
     belongs_to :plant, optional: true
-    belongs_to :plant, optional: true
+    belongs_to :group, optional: true
     has_many :datapoints, class_name: "datapoint", foreign_key: "reference_id"
 end
