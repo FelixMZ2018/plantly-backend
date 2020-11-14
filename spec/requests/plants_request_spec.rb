56ea2f542763c438718bd689ed853f2c2e29f2fd
Felix <FelixinLondon@googlemail.com>
2020-11-14 23:46:20 +0100

    adding to testsuite

--- spec/requests/plants_request_spec.rb
+++ spec/requests/plants_request_spec.rb
@@ -3,4 +3,14 @@
 require "rails_helper"
 
 RSpec.describe "Plants", type: :request do
+  it "is valid with a group" do
+    gr = Group.create
+    new_plant = Plant.new(group_id: gr.id)
+    expect(new_plant).to be_valid
+    gr.delete
+  end
+  it "is not valid without a group" do
+    new_plant_no_attributes = Plant.new
+    expect(new_plant_no_attributes).to_not be_valid
+  end
 end
