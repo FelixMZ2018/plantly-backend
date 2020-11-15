eec6292bc3485514bdf6786ccf9c4d1fc4918787
Felix <FelixinLondon@googlemail.com>
2020-11-15 23:49:09 +0100

    Adding Datapoint creation to testing

--- spec/requests/datapoint_request_spec.rb
+++ spec/requests/datapoint_request_spec.rb
@@ -2,6 +2,17 @@
 
 require "rails_helper"
 
+test_message = JSON.parse(
+  '{"Plant_Group": "Living_Room_"
+  ,
+  "sensor": [{"index": 0,"value": 755}
+  ,
+  {"index": 1,"value": 603},
+  {"index": 2,"value": 991},
+  {"index": 3,"value": 1023}]}'
+)
+
+puts test_message
 RSpec.describe Datapoint, type: :model do
   it "is valid with valid attributes" do
     expect(Datapoint.new(sensor_id: 1, value: 2)).to be_valid
@@ -14,4 +25,11 @@ RSpec.describe Datapoint, type: :model do
     dp_no_value = Datapoint.new(sensor_id: 1, value: nil)
     expect(dp_no_value).to_not be_valid
   end
+  # it "Should understand the Sensor Message" do
+  #   test_group = Group.new(name: "Living_Room_")
+  #   test_group.save
+  #   test_sensor = Sensor.create(group_id: test_group.id, hardware_id: "Living_Room_0")
+  #   interpret_message(test_message)
+  #   expect(Datapoint.last.value).to eq(1023)
+  # end
 end
