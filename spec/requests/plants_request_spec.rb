cbe0b18f696b029956d1a129184b262f397ea3ed
Felix <FelixinLondon@googlemail.com>
2021-03-04 00:01:50 +0100

    Added test dependencies

---
+++ spec/requests/plants_request_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Plants", type: :request do
+  it "is valid with a group" do
+    gr = Group.create(name: "Living Room", hardware_id: "living_room_1")
+    new_plant = Plant.new(group_id: gr.id)
+    expect(new_plant).to be_valid
+    gr.delete
+  end
+  it "is not valid without a group" do
+    new_plant_no_attributes = Plant.new
+    expect(new_plant_no_attributes).to_not be_valid
+  end
+end
