7858c1a23dede41d1b785f8c67b9cf6bf049cd92
Felix <FelixinLondon@googlemail.com>
2021-03-17 11:43:37 +0100

    Updated plant spec

--- app/javascript/components/Plants/ViewPlant.jsx
+++ app/javascript/components/Plants/ViewPlant.jsx
@@ -1,24 +1,32 @@
-import React, { useEffect,setState,useState } from 'react'
-import {useParams} from 'react-router-dom'
-import {axiosInstance} from '../../clients/axiosInstance'
+import React, { useEffect, setState, useState } from "react";
+import { useParams } from "react-router-dom";
+import { axiosInstance } from "../../clients/axiosInstance";
 
-function ViewPlant (props) {
-    const id = useParams()['id']
-    const [plant,setPlant] = useState({plant: null})
-    const [isLoaded,setIsLoaded] = useState({isLoaded: false})
+function ViewPlant(props) {
+  const id = useParams()["id"];
+  const [plant, setPlant] = useState({ plant: null });
+  const [isLoaded, setIsLoaded] = useState({ isLoaded: false });
 
-    useEffect(async() => {
-      const result = await axiosInstance.get(`/plants/${id}`,
-      { headers: {"Authorization" : `Bearer ${props.jwt}`} })
-      setPlant({plant: result.data})
-      
-      setIsLoaded({isLoaded: true})},[]);
+  useEffect(async () => {
+    const result = await axiosInstance.get(`/plants/${id}`, {
+      headers: { Authorization: `Bearer ${props.jwt}` },
+    });
+    setPlant({ plant: result.data });
 
-    console.log(useParams()['id'])
-        return(
-            <div className="ViewPlant bg-green-light flex-grow">
-              Name: {this.state.plant.name}
-              </div>
-        )
+    setIsLoaded({ isLoaded: true });
+  }, []);
+
+  console.log(useParams()["id"]);
+  if (isLoaded) {
+    return (
+      <div className="ViewPlant bg-green-light flex-grow">
+        Name: 
+      </div>
+    );
+  } else {
+    return (
+      <div>Loading! </div> 
+    )
+  }
 }
-export default ViewPlant
+export default ViewPlant;
--- spec/requests/plants_request_spec.rb
+++ spec/requests/plants_request_spec.rb
@@ -4,10 +4,10 @@ require "rails_helper"
 
 RSpec.describe "Plants", type: :request do
   it "is valid with a group" do
-    gr = Group.create(name: "Living Room", hardware_id: "living_room_1")
-    new_plant = Plant.new(group_id: gr.id)
+    test_user = User.create(username: "sample", password: "sample")
+    test_group = Group.create(name: "Living Room", hardware_id: "Living_Room_", user_id: test_user.id)
+    new_plant = Plant.new(group_id: test_group.id,user_id: test_user.id)
     expect(new_plant).to be_valid
-    gr.delete
   end
   it "is not valid without a group" do
     new_plant_no_attributes = Plant.new
