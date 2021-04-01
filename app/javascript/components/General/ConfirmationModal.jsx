6d5b6e60faa36161d452dbe8bb08200289c073f1
Felix <FelixinLondon@googlemail.com>
2021-04-01 20:46:28 +0200

    Styling of confirmation modal

--- app/javascript/components/General/ConfirmationModal.jsx
+++ app/javascript/components/General/ConfirmationModal.jsx
@@ -3,10 +3,13 @@ import React, { setState, useState, useEffect } from "react";
 
 export default function ConfirmationModal(props) {
   return (
-    <div>
-      {props.text}
-      <div onClick={props.function} className="flex justify-end pt-2">
-        <Button text="Delete this Plant" />
+    <div className="fixed w-full h-full bg-gray-200 flex justify-center items-center ">
+      <div className="absolute bg-white z-50 flex justify-center mx-auto p-5 rounded-lg">
+        <div className="justify-center text-center text-green-dark">{props.text}</div>
+        <br/>
+        <div onClick={props.function} className="flex justify-end pt-2">
+          <Button text="Delete this Plant" />
+        </div>
       </div>
     </div>
   );
