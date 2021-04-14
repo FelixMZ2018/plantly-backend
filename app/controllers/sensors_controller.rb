7469738984e6a6b75fff50b686d95effc6d3066b
Felix <FelixinLondon@googlemail.com>
2021-04-14 23:23:17 +0200

    ADD: Controller and router for adoptable sensors

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -3,10 +3,12 @@ class SensorsController < ApplicationController
     def listAdoptable
         @group = Group.where(user_id: @user.id).find(params[:id])
         @sensors = []
-        @group.each do | s | 
+        @group.sensors.each do | s |
+            if s.sensor_type === "soil_moisture" && s.plant.nil?
+                @sensors << s
+            end
         end
-
-
-    end
+        render json: @sensors
+      end
 
 end
