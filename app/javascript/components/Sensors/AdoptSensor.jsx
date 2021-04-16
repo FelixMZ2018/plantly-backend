40ef4e8d076b0213f5a1f7f0128c96f6dfd5fd8a
Felix <FelixinLondon@googlemail.com>
2021-04-16 23:26:52 +0200

    Adding Axios Call to Sensor Selection

--- app/javascript/components/Sensors/AdoptSensor.jsx
+++ app/javascript/components/Sensors/AdoptSensor.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export default function AdoptSensor() {
+    const [data, setData] = useState({ hits: [] });
+       
+        useEffect(async () => {
+          const result = await axios(
+            'https://hn.algolia.com/api/v1/search?query=redux',
+          );
+       
+          setData(result.data);
+        });
     return (
         <div>
             
