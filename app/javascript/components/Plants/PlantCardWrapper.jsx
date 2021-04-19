902252fd7838dca62059d14aad355b4119c1080f
Felix <FelixinLondon@googlemail.com>
2021-04-19 23:46:47 +0200

    Linter fixes

--- app/javascript/components/Plants/PlantCardWrapper.jsx
+++ app/javascript/components/Plants/PlantCardWrapper.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import PlantCard from './Plantcard'
-import {Link} from 'react-router-dom'
+import React from "react";
+import PlantCard from "./Plantcard";
+import { Link } from "react-router-dom";
 
-
-class PlantCardWrapper extends React.Component{
-    render(){
-        return(
-            <div className="PlantCardWrapper grid grid-cols-4">
-            {this.props.plants.map((plant) => (
+class PlantCardWrapper extends React.Component {
+  render() {
+    return (
+      <div className="PlantCardWrapper grid grid-cols-4">
+        {this.props.plants.map((plant) => (
           <Link key={plant.id} to={`/plants/${plant.id}`}>
-          <PlantCard key={plant.id} plant={plant}/>
-                </Link>
-              ))}
-              </div>
-        )
-    }
+            <PlantCard key={plant.id} plant={plant} />
+          </Link>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default PlantCardWrapper
+export default PlantCardWrapper;
--- app/javascript/components/Plants/Plantcard.jsx
+++ app/javascript/components/Plants/Plantcard.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import PlantCardSensor from '../Sensors/PlantCardSensor'
+import React from "react";
+import PlantCardSensor from "../Sensors/PlantCardSensor";
 
-class Plantcard extends React.Component{
-    render(){
-        return(
-            <div className="PlantCard cursor-pointer bg-white m-5 pb-3 rounded-xl shadow-2xl hover:shadow-none duration-300" >
-                <img src={this.props.plant.image_url}/>
-                <h2 className="text-textColor-darkgreen p-2">{this.props.plant.name}</h2>
-                {this.props.plant.sensors.map(function(sensor,index){
-                return <PlantCardSensor sensor={sensor}  key={index}/>
-        })}                
-            
-                
-                
-                </div>
-        )
-    }
+class Plantcard extends React.Component {
+  render() {
+    return (
+      <div className="PlantCard cursor-pointer bg-white m-5 pb-3 rounded-xl shadow-2xl hover:shadow-none duration-300">
+        <img src={this.props.plant.image_url} />
+        <h2 className="text-textColor-darkgreen p-2">
+          {this.props.plant.name}
+        </h2>
+        {this.props.plant.sensors.map(function (sensor, index) {
+          return <PlantCardSensor sensor={sensor} key={index} />;
+        })}
+      </div>
+    );
+  }
 }
-export default Plantcard
+export default Plantcard;
